4813f7e1cadc8da44d380ae08f274f9161c5d3c9
Stephen White <stephenwhite@northwesternmutual.com>
2020-04-16 08:22:40 -0500

    adding first component

---
+++ src/Quiz.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+let quizData = require('./quiz_data.json');
+
+class Quiz extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { quiz_position: 1 }
+  }
+  render() {
+    return
+      <div>
+        <div className="QuizQuestion">{quizData[1].intruction_text}</div>
+      </div>;
+  }
+}
+
+export default Quiz;
