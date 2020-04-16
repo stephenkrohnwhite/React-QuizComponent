6b4a28242ee9149b3c044776672e3e7a5a031dfb
Stephen White <stephenwhite@northwesternmutual.com>
2020-04-16 10:42:20 -0500

    finished

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,6 +14,9 @@ class Quiz extends Component {
       return { quiz_position: state.quiz_position + 1 }
     })
   }
+  handleResetClick() {
+    this.setState({ quiz_position: 1 })
+  }
   render() {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
@@ -24,6 +27,7 @@ class Quiz extends Component {
           <QuizQuestion 
             showNextQuestionHandler={ this.showNextQuestion.bind(this) }
             quiz_question={ quizData.quiz_questions[this.state.quiz_position - 1] }
+            resetClickHandler={this.handleResetClick.bind(this)}
           />
          ) }
       </div>
